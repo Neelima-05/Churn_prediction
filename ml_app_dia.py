ebc28ffccdc12ddf7166614ae2d8cf40d5f17d34
Neelima-05 <88318606+Neelima-05@users.noreply.github.com>
2021-08-26 09:59:10 +0530

    Add files via upload

--- ml_app_dia.py
+++ ml_app_dia.py
@@ -1,21 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+#import plotly.express as px
+#from plotly.subplots import make_subplots
+#import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import pickle
 
-pickle_out = open('XGBoost.pkl', 'wb')
-classifier = pickle.load(pickle_out)
+import pickle
+pickle_in = open('XGBoost.pkl', 'rb')
+classifier = pickle.load(pickle_in)
 
 st.sidebar.header('Churn Prediction')
 select = st.sidebar.selectbox('Select Form', ['Form 1'], key='2')
-if not st.sidebar.checkbox("Hide", True, key='2'):
+if not st.sidebar.checkbox("Hide", True, key='3'):
     st.title('Customer Churn Prediction')
     EmailsSent = st.text_input("Email Sent:")
     PUPDismiss = st.number_input("PUP dismiss:")
@@ -27,18 +27,20 @@ if not st.sidebar.checkbox("Hide", True, key='2'):
     ResumeUploaded = st.number_input("Resume Upload:")
     ViewAttachments = st.number_input("View Attachments:")
     CampaignsRun = st.text_input("Campaigns Run:")
-    SavedAI-JobMatchingCriteria = st.number_input("SavedAI-Job Matching Criteria:")
+    SavedAI = st.number_input("SavedAI-Job Matching Criteria:")
     NotesAdded = st.number_input("Notes added:")
     JobRecommendationList =  st.number_input("Job Recommendation List:")
-    AddToJob-CRM = st.number_input("AddToJob-CRM:")
+    AddToJob = st.number_input("AddToJob-CRM:")
     EventsShared = st.number_input("Events Shared:")
     TagsAdded = st.number_input("Tags Added:")
 
     submit = st.button('Predict')
 
     if submit:
-        prediction = classifier.predict([[EmailsSent,PUPDismiss,OverTime,SmsSent,ProfilesForwarded,EventsCreated,PastApplicant,ResumeUploaded,ViewAttachments,CampaignsRun,SavedAI-JobMatchingCriteria,NotesAdded,
-  JobRecommendationList,AddToJob-CRM,EventsShared,TagsAdded]])
+#        prediction = classifier.predict([EmailsSent,PUPDismiss,OverTime,SmsSent,ProfilesForwarded,EventsCreated,PastApplicant,ResumeUploaded,ViewAttachments,CampaignsRun,SavedAI,NotesAdded,
+ # JobRecommendationList,AddToJob,EventsShared,TagsAdded])
+        df = pd.DataFrame({'EmailsSent':EmailsSent,'PUPDismiss':PUPDismiss,'OverTime':OverTime,'SmsSent':SmsSent,'ProfilesForwarded':ProfilesForwarded,'EventsCreated':EventsCreated,'PastApplicant':PastApplicant,'ResumeUploaded':ResumeUploaded,'ViewAttachments':ViewAttachments,'CampaignsRun':CampaignsRun,'SavedAI':SavedAI,'NotesAdded':NotesAdded,'JobRecommendationList':JobRecommendationList,'AddToJob':AddToJob,'EventsShared':EventsShared,'TagsAdded':TagsAdded},index=[0])
+        prediction = classifier.predict(df)
         if prediction == 1:
             st.write('Congratulation the customer wont churn')
         else:
